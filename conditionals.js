d69b0edbd788ffdb8e01fcefde3d5e8099b8da23
Kuhle Vulangile <kuhlevulangile@gmail.com>
2025-10-31 12:26:06 +0200

    Basic Js  Conditionals

---
+++ conditionals.js
@@ -0,0 +1,16 @@
+score = 55;
+
+passingGrade = 60;
+
+if (score >= passingGrade) {
+    console.log("You passed!");
+} else {(score < passingGrade) 
+    console.log("You failed.");
+}
+
+if (score === 100) {
+    console.log("Perfect score!");
+} 
+else {
+    console.log("Keep trying!");
+}
